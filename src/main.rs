4d9efd45d383b0cd7fee66120111dd8196b63728
bello mahmud <blinkztyler@gmail.com>
2022-02-21 10:05:35 +0100

    my first project with rust

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+
+fn main() {
+    // println!("Hello, world!");
+    two_sums(&[2,3,4,5,6,7],9);
+}
+fn two_sums(test_array:  &[i64],num :i64) -> (){
+    
+        for i in IntoIterator::into_iter(test_array){
+             for n in test_array{
+                if i+n==num {
+                  println!("{:?}",[*i,*n]);
+                    
+                }
+            }
+          
+        }
+      
+    }
+   
+
